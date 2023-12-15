62fe14954097bf5d3ae6c40d1c1dddd6253e44c9
aaraniMathi <aaranimathii1234@gmail.com>
2023-12-15 19:39:56 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Timers from './Timers';
+
+function App() {
+  return (
+    <div className="App">
+      <Timers/>
+       
+    </div>
+  );
+}
+
+export default App;
